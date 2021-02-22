24c078935213222ea1361ab2f09ba811b36c001b
Hunter Krasa <hunterkrasa1@gmail.com>
2021-02-22 11:59:20 -0800

    feat: add search form

---
+++ src/components/SearchForm.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import {Typography} from "@material-ui/core";
+
+export default function SearchForm(props) {
+    const [searchValue, setSearchValue]
+
+
+    function handleSubmit(event) {
+
+
+    }
+
+
+
+    return (
+        <div> 
+            <form onSubmit={handleSubmit}>
+                <input placeholder="Type anything..." value={searchValue} onChange={(event) => setSearchValue(event.target.value)}/>
+                <button>Search</button>
+            </form>
+
+        </div>
+    )
+
+} 
+
+
+
