9bbaf7995d9cfea23f84d4099746dac704077b12
Ashley Buschhorn <abuschhorn@gmail.com>
2021-02-22 14:35:01 -0600

    refactor: add styles to SearchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,27 @@
 import React, {useState} from "react";
-import {Typography} from "@material-ui/core";
+import {makeStyles, Button} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+    root: {
+        textAlign: "center",
+        marginTop: 50,
+    },
+    button: {
+        padding: 10,
+        margin: 10,
+        color: "red",
+        alignSelf: "center",
+    },
+    input: {
+        padding: 10,
+        margin: 10,
+        width: "50%",
+    }
+}))
 
 export default function SearchForm(props) {
+    const classes = useStyles()
     const [searchValue, setSearchValue] = useState("");
-
 
     function handleSubmit(event) {
 
@@ -13,10 +31,10 @@ export default function SearchForm(props) {
 
 
     return (
-        <div> 
-            <form onSubmit={handleSubmit}>
-                <input placeholder="Type anything..." value={searchValue} onChange={(event) => setSearchValue(event.target.value)}/>
-                <button>Search</button>
+        <div className={classes.root}> 
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <input className={classes.input} placeholder="Type anything..." value={searchValue} onChange={(event) => setSearchValue(event.target.value)} />
+                <div><Button className={classes.button} variant="contained">Search</Button></div>
             </form>
 
         </div>
