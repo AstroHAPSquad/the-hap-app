0f486b44783b36367588ba8a09d443648de69dc7
parkerbath <pbath@lion.lmu.edu>
2021-03-01 12:02:27 -0800

    updated dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,59 +9,31 @@ import Paper from '@material-ui/core/Paper';
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import {data} from "../mockdata/data"
-
-
-const tileData = [
- {img: "https://picsum.photos/id/0/5616/3744"},
- {img: "https://picsum.photos/id/1/5616/3744"},
- {img: "https://picsum.photos/id/10/2500/1667"},
- {img: "https://picsum.photos/id/100/2500/1656"},
- {img: "https://picsum.photos/id/1000/5626/3635"},
- {img: "https://picsum.photos/id/1001/5616/3744"},
- {img: "https://picsum.photos/id/1002/4312/2868"},
- {img: "https://picsum.photos/id/1003/1181/1772"},
- {img: "https://picsum.photos/id/1004/5616/3744"},
- {img: "https://picsum.photos/id/1005/5760/3840"},
- {img: "https://picsum.photos/id/1006/3000/2000"},
- {img: "https://picsum.photos/id/1008/5616/3744"},
- {img: "https://picsum.photos/id/1009/5000/7502"},
- {img: "https://picsum.photos/id/101/2621/1747"},
- {img: "https://picsum.photos/id/1010/5184/3456"},
- {img: "https://picsum.photos/id/1011/5472/3648"},
- {img: "https://picsum.photos/id/1012/3973/2639"},
- {img: "https://picsum.photos/id/1013/4256/2832"},
- {img: "https://picsum.photos/id/1014/6016/4000"},
- {img: "https://picsum.photos/id/1015/6000/4000"},
- {img: "https://picsum.photos/id/1016/3844/2563"},
- {img: "https://picsum.photos/id/1018/3914/2935"},
- {img: "https://picsum.photos/id/1019/5472/3648"},
- {img: "https://picsum.photos/id/102/4320/3240"},
- {img: "https://picsum.photos/id/1020/4288/2848"},
- {img: "https://picsum.photos/id/1021/2048/1206"},
- {img: "https://picsum.photos/id/1022/6000/3376"},
- {img: "https://picsum.photos/id/1023/3955/2094"},
- {img: "https://picsum.photos/id/1024/1920/1280"},
- {img: "https://picsum.photos/id/1025/4951/3301"}
- ];
  
 // work on styles, then work on hovering feature, then work on having the image be coming just from the mockdata
 
 
  const useStyles = makeStyles(theme => ({
   root: {
-
+    padding: 60,
+    paddingTop: 40
   },
   grid: {
     width: 500,
-    height: 450
+    height: 450,
+
   }
 }));
 export default function Dashboard(props) {
   const classes = useStyles();
   const {posts} = props;
   return (
+    <>
+    <div style={{display: "flex", justifyContent: "center", paddingTop: 20}}>
+    <h1>Dashboard</h1>
+    </div>
     <div className={classes.root}>
-      <Grid container align= "center" alignItems= "center">
+      <Grid container align= "center" alignItems= "center" spacing = {9}>
           {posts.map(tile => (
             <Grid item xs={4}>
               <img src={tile.img} height = { 400 } />
@@ -69,9 +41,8 @@ export default function Dashboard(props) {
         ))}
       </Grid>
     </div>
+    </>
   );
 } 
-
-
 
 
